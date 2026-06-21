c2634b8e4d8f2b21ba2a257f86e851d2e48314c0
Sadsaurio <mattquezada0@gmail.com>
2026-06-20 19:40:37 -0600

    Primer commit

---
+++ proyecto.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;        
+
+ struct pregunta{
+    string  cuestion;//pregunta pero ps para no repetir con el nombre de la estructura
+        string opcion1;
+        string opcion2;
+        string opcion3;
+        string opcion4;
+    int correcta;
+    int puntaje;
+    pregunta *sig;
+    pregunta *ant;
+ };
+
+
+
+int main() {
+
+
+
+
+}
